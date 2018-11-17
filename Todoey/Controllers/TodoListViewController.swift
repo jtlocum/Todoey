5706371429e645d77918efb70ea6e26584d34892
Thomas Matthews <ThomasMatthews@thomass-macbook-pro.local>
2018-11-17 16:22:42 +0000

    just before front end changes

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -92,6 +92,7 @@ class TodoListViewViewController: UITableViewController {
                 try self.realm.write {
                     let newItem = Item()
                     newItem.title = textField.text!
+                    newItem.dateCreated = Date()
                     currentCategory.items.append(newItem)
                 }
                 } catch {
@@ -134,7 +135,9 @@ class TodoListViewViewController: UITableViewController {
 extension TodoListViewViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
     
-        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "title", ascending: true)
+        todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
         
     }
     
